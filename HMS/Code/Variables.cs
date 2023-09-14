8d7b9bb464bbf3ba5995605ff7b15fb22a8427f3
Divya Swarnkar <kdivyaswarnkar@gmail.com>
2023-09-14 16:21:39 +0530

    Frontend and user Authentications

---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Divya International Resort";
+        public const string HotelShortName = "Divya";
+        public const string HotelNameAbbreviate = "DIR";
+    }
+}
